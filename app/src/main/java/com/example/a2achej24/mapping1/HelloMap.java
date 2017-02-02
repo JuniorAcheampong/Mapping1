309b86cf6dbc2732ea9e991fe02486a30c12e0ba
Junior <2achej24@solent.ac.uk>
2017-02-02 10:58:28 +0000

    Final changes

--- app/src/main/java/com/example/a2achej24/mapping1/HelloMap.java
+++ app/src/main/java/com/example/a2achej24/mapping1/HelloMap.java
@@ -3,19 +3,21 @@ package com.example.a2achej24.mapping1;
 import android.app.Activity;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import org.osmdroid.config.Configuration;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
 
 public class HelloMap extends Activity
-{
+    implements View.OnClickListener {
 
     MapView mv;
 
-    public void onCreate(Bundle savedInstanceState)
-    {
 
+    protected void OnCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -24,12 +26,32 @@ public class HelloMap extends Activity
         Configuration.getInstance().load
                 (this, PreferenceManager.getDefaultSharedPreferences(this));
 
-        mv = (MapView)findViewById(R.id.map1);
+        mv = (MapView) findViewById(R.id.map1);
 
         mv.setBuiltInZoomControls(true);
         mv.getController().setZoom(14);
-        mv.getController().setCenter(new GeoPoint(40.1,- 22.5));
+        mv.getController().setCenter(new GeoPoint(40.1, -22.5));
+
+        Button SubmitButton = (Button) findViewById(R.id.SubmitButton);
+        SubmitButton.setOnClickListener(this);
+    }
+
+    public void onClick(View v) {
+
+        //retrieve the latitude
+        EditText LatitudeEditText = (EditText) findViewById(R.id.latitudeEditText);
+        double latitude = Double.parseDouble(LatitudeEditText.getText().toString());
+
+        // retrieve the longitude
+        EditText LongitudeEditText = (EditText) findViewById(R.id.LongitudeEditText);
+        double longitude = Double.parseDouble(LatitudeEditText.getText().toString());
+
+        // set the map to the new latitude and longitude
+
+        
     }
 }
+
+
 
 
